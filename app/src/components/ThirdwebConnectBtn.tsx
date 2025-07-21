89c6e9f57f6ed759922207e0310851319f0ce5a9
VinGitonga <gitongavincent64@gmail.com>
2025-07-21 06:22:47 +0300

    Implement thirdweb configuration store functionality

---
+++ app/src/components/ThirdwebConnectBtn.tsx
@@ -0,0 +1,14 @@
+import { useThirdwebStore } from "@/hooks/store/useThirdwebStore";
+import { defineChain } from "thirdweb";
+import { ConnectButton } from "thirdweb/react";
+import { createWallet } from "thirdweb/wallets";
+import { etherlinkTestnet } from "viem/chains";
+
+const wallets = [createWallet("io.metamask")];
+
+const ThirdwebConnectBtn = () => {
+	const { client } = useThirdwebStore();
+	return <ConnectButton client={client!} wallets={wallets} connectModal={{ size: "compact" }} chain={defineChain(etherlinkTestnet.id)} />;
+};
+
+export default ThirdwebConnectBtn;
