7cd8245ed98dc264301869ef9691da1e3a6bb65a
VinGitonga <gitongavincent64@gmail.com>
2025-07-27 09:55:59 +0300

    Add interactive charts and data table components to Dashboard; enhance UI with new sidebar and layout components; update dependencies in package.json and yarn.lock

---
+++ app/src/layouts/HomeLayout.tsx
@@ -0,0 +1,24 @@
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+
+import { AppSidebar } from "@/components/layouts/app-sidebar";
+
+import { Outlet } from "react-router";
+
+const HomeLayout = () => {
+	return (
+		<SidebarProvider
+			style={
+				{
+					"--sidebar-width": "calc(var(--spacing) * 72)",
+					"--header-height": "calc(var(--spacing) * 12)",
+				} as React.CSSProperties
+			}>
+			<AppSidebar variant="inset" />
+			<SidebarInset>
+				<Outlet />
+			</SidebarInset>
+		</SidebarProvider>
+	);
+};
+
+export default HomeLayout;
--- app/src/layouts/RootLayout.tsx
+++ app/src/layouts/RootLayout.tsx
@@ -1,6 +1,7 @@
 import useAppService from "@/hooks/useAppService";
 import { HeroUIProvider, ToastProvider } from "@heroui/react";
 import { Outlet, useHref, useNavigate } from "react-router";
+import { Toaster as SonnerToaster } from "@/components/ui/sonner"
 
 const RootLayout = () => {
 	const navigate = useNavigate();
@@ -9,6 +10,7 @@ const RootLayout = () => {
 	return (
 		<HeroUIProvider navigate={navigate} useHref={useHref}>
 			<ToastProvider />
+			<SonnerToaster />
 			<div className="min-h-screen antialiased transition-colors ease-in-out duration-200 font-nunito">
 				<Outlet />
 			</div>
